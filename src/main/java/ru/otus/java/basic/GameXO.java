d5f4919c55b49ed11e0371ff2b4cac5473e52400
ilia_19011991 <ilia_19011991@mail.ru>
2025-02-16 19:19:58 +0300

    xo game ready

---
+++ src/main/java/ru/otus/java/basic/GameXO.java
@@ -0,0 +1,12 @@
+package ru.otus.java.basic;
+
+public class GameXO {
+    public static void main(String[] args) {
+        System.out.println("game started");
+        System.out.println("x turn");
+        System.out.println("o turn");
+        System.out.println("check win");
+        System.out.println("try again");
+        System.out.println("end game");
+    }
+}
