eb2ec2e09a7496b392a26f1d18e384df4c22c433
ilia_19011991 <ilia_19011991@mail.ru>
2025-02-16 19:25:39 +0300

    ai стал еще умнее

--- src/main/java/ru/otus/java/basic/GameXO.java
+++ src/main/java/ru/otus/java/basic/GameXO.java
@@ -12,9 +12,10 @@ public class GameXO {
     }
 
     public static void aiTurn() {
-        System.out.println("==");
+        System.out.println("=  =");
         System.out.println(1);
         System.out.println(2);
-        System.out.println("==");
+        System.out.println(3);
+        System.out.println("=  =");
     }
 }
