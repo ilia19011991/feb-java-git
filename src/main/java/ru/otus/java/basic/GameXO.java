874497b24b63d4802b286c2eb9fe019c17bf7456
ilia_19011991 <ilia_19011991@mail.ru>
2025-02-16 19:30:30 +0300

    добавлен gui

--- src/main/java/ru/otus/java/basic/GameXO.java
+++ src/main/java/ru/otus/java/basic/GameXO.java
@@ -1,7 +1,12 @@
 package ru.otus.java.basic;
 
+import javax.swing.*;
+
 public class GameXO {
     public static void main(String[] args) {
+        JFrame jFrame = new JFrame();
+        jFrame.setBounds(100, 100, 200, 200);
+        jFrame.setVisible(true);
         System.out.println("game started");
         System.out.println("x turn");
         System.out.println("o turn");
