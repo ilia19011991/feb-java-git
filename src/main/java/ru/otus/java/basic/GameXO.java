ff7049c452e47c8d6966bda51c61f2f28eb49dc3
ilia_19011991 <ilia_19011991@mail.ru>
2025-02-16 19:24:42 +0300

    ai improved

--- src/main/java/ru/otus/java/basic/GameXO.java
+++ src/main/java/ru/otus/java/basic/GameXO.java
@@ -5,8 +5,16 @@ public class GameXO {
         System.out.println("game started");
         System.out.println("x turn");
         System.out.println("o turn");
+        aiTurn();
         System.out.println("check win");
         System.out.println("try again");
         System.out.println("end game");
+    }
+
+    public static void aiTurn() {
+        System.out.println("==");
+        System.out.println(1);
+        System.out.println(2);
+        System.out.println("==");
     }
 }
