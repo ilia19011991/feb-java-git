59d29fb7a2ed6c20cef2812746eaf5d448773368
ilia_19011991 <ilia_19011991@mail.ru>
2025-02-16 18:14:44 +0300

    Реализован метод drawSquare()

--- src/main/java/ru/otus/java/basic/App.java
+++ src/main/java/ru/otus/java/basic/App.java
@@ -3,5 +3,14 @@ package ru.otus.java.basic;
 public class App {
     public static void main(String[] args) {
         System.out.println("Hello World!!!");
+        drawSquare();
+    }
+
+    public static void drawSquare() {
+        System.out.println("=====");
+        System.out.println("=   =");
+        System.out.println("= 5 =");
+        System.out.println("=   =");
+        System.out.println("=====");
     }
 }
